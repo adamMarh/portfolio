8a64d7db87541c8e3945915c0150b03565c03766
Adam <adam.mrh@outlook.com>
2026-09-07 12:35:45 -0400

    Fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: provess.env.VITE_BASE_PATH || "/portfolio/tree/prod"
+  base: provess.env.VITE_BASE_PATH || "/portfolio"
 });
